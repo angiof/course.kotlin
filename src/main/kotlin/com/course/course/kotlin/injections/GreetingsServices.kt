6447c4a7cbfac12d39cd7da71db01361b712ba6c
angelo ferretti <31105678+angiof@users.noreply.github.com>
2022-08-23 16:58:50 +0200

    salva primo commit

--- src/main/kotlin/com/course/course/kotlin/injections/GreetingsServices.kt
+++ src/main/kotlin/com/course/course/kotlin/injections/GreetingsServices.kt
@@ -4,5 +4,6 @@ import org.springframework.stereotype.Service
 
 @Service
 class GreetingsServices {
+    //fun che permette il servizio di dipendences
     fun retriveSaluti(name: String) = "ciao sir $name"
 }
