eae46f3ada846d35c66ce1cc49a01189a997c9ee
angelo ferretti <31105678+angiof@users.noreply.github.com>
2022-08-23 18:20:57 +0200

    add ambiente di sviluppo
    aggiunto ambiente di sviluppo prod e default

--- src/main/kotlin/com/course/course/kotlin/controler/GretingControler.kt
+++ src/main/kotlin/com/course/course/kotlin/controler/GretingControler.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/v1/gree")
+@RequestMapping("/v1/greetings")
 class GretingControler(val greetingsServices: GreetingsServices) {
 
     @GetMapping("/{name}")
--- src/main/kotlin/com/course/course/kotlin/injections/GreetingsServices.kt
+++ src/main/kotlin/com/course/course/kotlin/injections/GreetingsServices.kt
@@ -1,9 +1,17 @@
 package com.course.course.kotlin.injections
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class GreetingsServices {
+
+
+    @Value("\${message}")
+    lateinit var name2: String
+
     //fun che permette il servizio di dipendences
-    fun retriveSaluti(name: String) = "ciao sir $name"
+    fun retriveSaluti(name: String) = "ciao sir $name $name2,"
+
+
 }
